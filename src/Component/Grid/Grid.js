3c461473359a8e76d2e98c91b470c5ede033e89d
Elena Izotova <elm57650@gmail.com>
2021-06-15 19:21:36 +0700

    попытка сделать пагинацию

--- src/Component/Grid/Grid.js
+++ src/Component/Grid/Grid.js
@@ -1,13 +1,12 @@
-import { Row, Col, Pagination, Card, Button } from "react-bootstrap";
-import {BrowserRouter as Router, Switch, Route, Link, useParams, useLocation, useHistory} from "react-router-dom";
-// import GridItem from "./GridItem";
+import { Row, Col, Pagination, Card } from "react-bootstrap";
+import {Route, Link, useParams, useLocation, useHistory} from "react-router-dom";
 import { Container } from "react-bootstrap";
-
+import { Component, useState } from "react";
 import  Recipe from "../Recipe/Recipe";
-import { getRecipes, getRecipesFromServer } from '../../Models/Recipets';
+import { getRecipesFromServer } from '../../Models/Recipets';
 
 function useQuery() {
-     return new URLSearchParams(useLocation().search);
+  return new URLSearchParams(useLocation().search);
 }
 
 function Grid() {
@@ -37,11 +36,10 @@ function Grid() {
         <span>Page №{pageNumber}</span>
         <Link to={linkToNextPage}>NextPage</Link>
 
-        <Pages className="section-resipes__pages" totalPages={response.totalPages}> </Pages>
+        <Pages className="section-resipes__pages" totalPages={response}> </Pages>
       </Container>
     )
 }
-
 
 function GridItem({ recipe }) {
     const link = `recipes/cards/${recipe.id}`;
@@ -61,24 +59,113 @@ function GridItem({ recipe }) {
 }
 
 
-function Pages({ totalPages }) {
-    let active = 1;
-    let items = [];
-    for (let page = 1; page <= totalPages; page++) {
+function Pages(){
+  let query = useQuery();
+  const pageNumber = parseInt(query.get("page") || '0');
+  const response = getRecipesFromServer(pageNumber);
+  
+  let {totalPages,nextPage,prevPage,currentPage, result} = response;
+  console.log(currentPage)
+  console.log(result)
+  let page = currentPage;
+  let active = page;
+  let items = [];
+
+  function handleClick (e) {
+    e.preventDefault();
+    page = currentPage;
+    console.log(page)
+  }
+  
+  function handleClickNext (e) {
+    e.preventDefault();
+    page = nextPage;
+    return result;
+  }
+  
+  function handleClickPrev (e) {
+    e.preventDefault();
+      page = prevPage;
+      currentPage = prevPage;
+  }
+
+  for (let page = 1; page <= totalPages; page++) {
       items.push(
-          <Pagination.Item key={page} active={page === active}>
+          <Pagination.Item key={page} active={page === active} onClick={handleClick}>
               {page}
         </Pagination.Item>,
       );
-    }
+  }
 
-    return(
+  return (
     <Pagination className="pagination justify-content-center pages" variant="outline-secondary">
-      <Pagination.Prev className="outline-secondary">Предыдущая </Pagination.Prev>
-     {items}
-     <Pagination.Next >Следующая</Pagination.Next>
+      <Pagination.Prev className="outline-secondary" onClick={handleClickPrev}>Предыдущая </Pagination.Prev>
+        {items}
+     <Pagination.Next onClick={handleClickNext}>Следующая</Pagination.Next>
     </Pagination>
-    )
-}
+  )
+};
 
 export default Grid;
+
+
+
+// class Page extends Component{
+// constructor() {
+//   super();
+//   this.state = {
+//     todos: [&#39;a&#39;,&#39;b&#39;,&#39;c&#39;,&#39;d&#39;,&#39;e&#39;,&#39;f&#39;,&#39;g&#39;],
+//     currentPage: 1,
+//     todosPerPage: 3
+//   };
+//   this.handleClick = this.handleClick.bind(this);
+// }
+
+// handleClick(event) {
+//   this.setState({
+//     currentPage: Number(event.target.id)
+//   });
+// }
+
+// render() {
+//   const { todos, currentPage, todosPerPage } = this.state;
+
+//   // Logic for displaying todos
+//   const indexOfLastTodo = currentPage * todosPerPage;
+//   const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
+//   const currentTodos = todos.slice(indexOfFirstTodo, indexOfLastTodo);
+
+//   const renderTodos = currentTodos.map((todo, index) => {
+//     return <li key={index}>{todo}</li>;
+//   });
+
+//   // Logic for displaying page numbers
+//   const pageNumbers = [];
+//   for (let i = 1; i <= Math.ceil(todos.length / todosPerPage); i++) {
+//     pageNumbers.push(i);
+//   }
+
+//   const renderPageNumbers = pageNumbers.map(number => {
+//     return (
+//       <li
+//         key={number}
+//         id={number}
+//         onClick={this.handleClick}
+//       >
+//         {number}
+//       </li>
+//     );
+//   });
+
+//   return (
+//     <div>
+//       <ul>
+//         {renderTodos}
+//       </ul>
+//       <ul id="page-numbers">
+//         {renderPageNumbers}
+//       </ul>
+//     </div>
+//   );
+// }
+// }
--- src/Models/Recipets.js
+++ src/Models/Recipets.js
@@ -17,9 +17,6 @@ const images = [
   'https://images.pexels.com/photos/1755785/pexels-photo-1755785.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
   'https://images.pexels.com/photos/793762/pexels-photo-793762.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
   'https://images.pexels.com/photos/6420305/pexels-photo-6420305.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-
-  
-
 ]
 
 
@@ -52,7 +49,7 @@ export function getRecipesFromServer(page) {
     totalPages: TOTAL_PAGES,
     nextPage: page + 1,
     prevPage: page - 1,
-    curretPage: page,
+    currentPage: page,
     result: getRecipes(page),
   }
 }
